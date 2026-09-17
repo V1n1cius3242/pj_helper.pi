659727c3029831737ce5aafd377573438ff40c22
K1d0zx <kaua14022010@gmail.com>
2026-09-17 14:13:45 -0300

    Sync changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
- sdd
+const http = require('http')
+const url = require('url')
+
+const callback = (req,res)=>{
+
+    var rota = url.parse(req.url,true)
+    res.writeHead(200,{'content-type':'application/json;charset=utf-8'})
+
+}
+
+var server = http.createServer(callback)
+
+server.listen(3000)
